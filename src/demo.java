1a5e091cdeac27c92973313044b928c403983ccf
Kranthi1992 <kittukranthi18@gmail.com>
2017-06-20 09:08:30 -0700

    1

---
+++ src/demo.java
@@ -0,0 +1,18 @@
+
+
+public class demo {
+	{
+		String name= "kittu";
+	}
+
+    
+    public static void main(String[] args) {
+        System.out.println(" demo.x");
+    }
+    
+
+    }
+
+    
+		
+	
