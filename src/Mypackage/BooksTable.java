ea50ddae24fa1a609a4eccb6be03e2647c5bb7eb
Kranthi1992 <kittukranthi18@gmail.com>
2017-06-20 09:04:01 -0700

    <Assignment5>

---
+++ src/Mypackage/BooksTable.java
@@ -0,0 +1,27 @@
+package Mypackage;
+
+public class BooksTable {
+	
+ public static void main(String[] args) {
+
+					System.out.println("column ID\tauthor\tbookname\tcategory");
+
+					int bookarray[]={};
+
+					
+
+					for(int a=1;a<bookarray.length;a++) {
+
+						System.out.println(a + "\t" + bookarray[a]);
+
+					}
+
+			}
+
+			
+
+
+
+		}
+
+
